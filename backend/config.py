96070ff65afa568afdb418ad9de2d45f4b356446
95percent <64401362+95percent@users.noreply.github.com>
2022-06-18 19:09:09 +0800

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-class Config(object):
-    pass
- 
-class DevConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI="mysql+pymysql://username:password@host:port/dbname"
-    #SQLALCHEMY_ECHO = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
