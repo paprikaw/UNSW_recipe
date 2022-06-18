ffc4e0235fd0ba97e1ece97bfecd4f556aa4fa96
Eric Bai <baixu.must@gmail.com>
2022-06-18 20:46:17 +0800

    updates frontend code

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/assets/css/index.css';
 import App from '@/pages/home/App';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
