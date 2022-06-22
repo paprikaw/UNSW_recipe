12128cdf3680321726ea600836e275c29512b9a8
Su Ann Khoo <z5209174@ad.unsw.edu.au>
2022-06-22 15:42:21 +1000

    U42-23 adding sql schema and stubs in server

---
+++ back-end/schema.sql
@@ -0,0 +1,26 @@
+create table Accounts (
+    accountId       serial primary key,
+    username        varchar(10) not null,
+    email           varchar(320) not null,
+    password        varchar(256) not null,
+    unique (email)
+)
+
+create table Sessions (
+    sessionId       serial primary key,
+    token           varchar(256) not null,
+    accountId       integer not null,
+    foreign key (accountId) references Accounts(accountId)
+)
+
+create table Categories (
+    categoryId      serial primary key,
+    categoryName    text not null
+)
+
+create table Ingredients (
+    ingredientId    serial primary key,
+    ingredientName  text not null,
+    categoryId      integer not null,
+    foreign key (categoryId) references Categories(categoryId)
+)
