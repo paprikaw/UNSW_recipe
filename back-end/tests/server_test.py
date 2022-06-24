d2b4766d273fd4772c2b2378212d9484547b8774
Su Ann <106937806+su-ann@users.noreply.github.com>
2022-06-24 12:27:53 +1000

    Update server_test.py

--- back-end/tests/server_test.py
+++ back-end/tests/server_test.py
@@ -6,8 +6,8 @@ from common import url, reset_server
 def test_signup_success():
     reset_server()
 
-    user1 = json.loads(requests.post(url + 'sign_up', json={'username': 'user1', 'email': 'user1@gmail.com', 'password': '123'}).text)
-    user2 = json.loads(requests.post(url + 'sign_up', json={'username': 'user2', 'email': 'user2@gmail.com', 'password': '123'}).text)
+    user1 = json.loads(requests.post(url + 'signup', json={'username': 'user1', 'email': 'user1@gmail.com', 'password': '123'}).text)
+    user2 = json.loads(requests.post(url + 'signup', json={'username': 'user2', 'email': 'user2@gmail.com', 'password': '123'}).text)
 
     assert user1 == {}
     assert user2 == {}
@@ -15,8 +15,8 @@ def test_signup_success():
 def test_signup_failure_on_duplicate_email():
     reset_server()
 
-    user1 = json.loads(requests.post(url + 'sign_up', json={'username': 'user1', 'email': 'user1@gmail.com', 'password': '123'}).text)
-    user2 = json.loads(requests.post(url + 'sign_up', json={'username': 'user2', 'email': 'user1@gmail.com', 'password': '123'}).text)
+    user1 = json.loads(requests.post(url + 'signup', json={'username': 'user1', 'email': 'user1@gmail.com', 'password': '123'}).text)
+    user2 = json.loads(requests.post(url + 'signup', json={'username': 'user2', 'email': 'user1@gmail.com', 'password': '123'}).text)
 
     assert user1 == {}
     assert user2['error'] == 'Email already in use.'
