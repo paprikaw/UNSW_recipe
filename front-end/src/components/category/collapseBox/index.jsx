0a14fd6d207999646d96e1a6323a47b72c4fc845
Eric Bai <baixu.must@gmail.com>
2022-06-29 21:58:31 +0800

    U42-38 refine the category animation

--- front-end/src/components/category/collapseBox/index.jsx
+++ front-end/src/components/category/collapseBox/index.jsx
@@ -1,48 +1,49 @@
- import {React} from 'react'
-import useCollapse from 'react-collapsed'
-import { Card, Button, Spin, Space } from 'antd'
-import SelectButton from './selectButton'
-import './index.scss'
+import { React } from "react";
+import useCollapse from "react-collapsed";
+import { Card, Button, Spin, Space } from "antd";
+import SelectButton from "./selectButton";
+import "./index.scss";
 
-function CollapseBox (props) {
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse({});
-  const newDic = {}
-  const {data, title, selectState, setSelectState} = props;
-  data.map(name => newDic[name] = false);
+function CollapseBox(props) {
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse({
+    collapsedHeight: "80",
+  });
+  const newDic = {};
+  const { data, title, selectState, setSelectState } = props;
+  data.map((name) => (newDic[name] = false));
+
   return (
-    <Card title={title} hoverable={true} className={'collapse-card'}extra={
-            <Button type='primary' {...getToggleProps()} >
-              {isExpanded ? 'Collapse' : 'Expand'}
-            </Button>
-        } 
-        style={{ margin: '0 auto' }}
-        >
-      {
-        isExpanded ?
-        <div style={{height: '300px'}} {...getCollapseProps()}>
+    <Card
+      title={title}
+      hoverable={true}
+      class={"collapse-card"}
+      style={{ margin: "0 auto" }}
+      actions={[
+        <Button {...getToggleProps()} type="link">
+          {isExpanded ? "Collapse" : "Expand"}
+        </Button>,
+      ]}
+    >
+      <div style={{ height: "300px" }} {...getCollapseProps()}>
         <Space size={[8, 16]} wrap>
-          {
-            selectState ?
-          data.map(name => {
-            return <SelectButton selectState={selectState} setSelectState={setSelectState} buttonText={name} categoryKey={title}/>})
-            :
-          <Spin tip="Loading..."/>
-          } 
-          </Space>
-        </div>
-        :
-        <Space size={[8, 16]} wrap>
-          {
-            selectState ?
-          data.slice(0, 6).map(name => {
-            return <SelectButton selectState={selectState} setSelectState={setSelectState} buttonText={name}/>})
-            :
-          <Spin tip="Loading..."/>
-          } 
-          </Space>
-      }
+          {selectState ? (
+            data.map((name) => {
+              return (
+                <SelectButton
+                  selectState={selectState}
+                  setSelectState={setSelectState}
+                  buttonText={name}
+                  categoryKey={title}
+                />
+              );
+            })
+          ) : (
+            <Spin tip="Loading..." />
+          )}
+        </Space>
+      </div>
     </Card>
-  )
+  );
 }
 
- export default CollapseBox;
+export default CollapseBox;
--- front-end/src/pages/register/index.jsx
+++ front-end/src/pages/register/index.jsx
@@ -1,89 +1,98 @@
-
 /* The login page start up code is from: https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/templates/sign-in/SignIn.js */
-import React from 'react';
-import { Button, Checkbox, Form, Input, Avatar, Space, Badge} from 'antd';
-import { LockTwoTone } from '@ant-design/icons';
-import { Link,useNavigate } from 'react-router-dom';
-import './index.scss';
+import React from "react";
+import { Button, Checkbox, Form, Input, Avatar, Space, Badge } from "antd";
+import { LockTwoTone } from "@ant-design/icons";
+import { Link, useNavigate } from "react-router-dom";
+import "./index.scss";
 
 const Register = () => {
-
   const navigate = useNavigate();
 
-  async function handleOnFinsh (values) {
-    const response = await fetch('http://localhost:8080/signup', {
-      method: 'POST',
+  function handleOnFinsh(values) {
+    console.log("here");
+    fetch("http://localhost:8080/signup", {
+      method: "POST",
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
       },
       body: JSON.stringify({
         email: values.email,
         password: values.password,
         username: values.username,
+      }),
+    })
+      .then((v) => {
+        return v.json();
       })
-    });
-    const data = await response.json();
-    console.log(data);
-    navigate('/');
+      .then((data) => {
+        console.log(data);
+      });
+    console.log("here2");
+    // const data =  response.json();
+    // console.log(data);
+    // navigate('/');
   }
 
   return (
     <div>
-      <Avatar size={64} icon={<LockTwoTone />} className='loginIcon'/>
+      <Avatar size={64} icon={<LockTwoTone />} className="loginIcon" />
       <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 8 }}
-      initialValues={{ remember: true }}
-      onFinish={handleOnFinsh}
-      // onFinishFailed={onFinishFailed}
-      autoComplete="off"
-  >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Please input your username!' }]}
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+        initialValues={{ remember: true }}
+        onFinish={handleOnFinsh}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
 
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+          <Link to="/">
+            <Badge dot>
+              <a href="#">Already have an account? Login</a>
+            </Badge>
+          </Link>
+        </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-        <Checkbox>Remember me</Checkbox>
-        <Link to='/'>
-          <Badge dot>
-            <a href="#">Already have an account? Login</a>
-          </Badge>
-        </Link>
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 8 }} >
-        <Space size='large' >
-        <Button type="primary" htmlType='submit'>
-          Register
-        </Button>
-        </Space>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
+          <Space size="large">
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
 
 /* Handler function */
-export default Register
+export default Register;
