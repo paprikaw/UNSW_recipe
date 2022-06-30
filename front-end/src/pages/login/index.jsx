783eac027ea61a3121b5b875ed2af871a9bbfcdb
Eric Bai <baixu.must@gmail.com>
2022-06-30 14:54:55 +0800

    quick fix

--- front-end/src/pages/login/index.jsx
+++ front-end/src/pages/login/index.jsx
@@ -62,7 +62,7 @@ const Login = () => {
           { required: true, message: "Please input your password!" },
           {
             pattern: /^[A-Z]((?![^a-z]+$)(?!\D+$).{5,14}$)$/,
-            message: 'Length should be 8-15 characters and include at least a Uppercase and lowercer case letter'
+            message: 'Length should be 6-15 characters and include at least a Uppercase and lowercer case letter'
           }
         ]}
       >
