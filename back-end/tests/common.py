0583891bc7e71afd3713ad4029f7adc3dc0d264f
Su Ann Khoo <z5209174@ad.unsw.edu.au>
2022-07-13 22:38:10 +1000

    U42-43 implementing recipe search

--- back-end/tests/common.py
+++ back-end/tests/common.py
@@ -3,9 +3,11 @@ import requests
 
 port = 8080
 url = f"http://localhost:{port}/"
-thumbnailDir = "./../src/imgs/thumbnails"
 
 def reset_server():
+    thumbnails = os.path.abspath(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)),'src/imgs/thumbnails')
+    )
     requests.delete(url + 'reset')
-    for file in os.scandir(thumbnailDir):
+    for file in os.scandir(thumbnails):
         os.remove(file.path)
