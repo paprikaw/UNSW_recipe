fc1995f4f78d89509875ad62fb889ef20bd1c4d6
Su Ann Khoo <z5209174@ad.unsw.edu.au>
2022-07-14 14:01:09 +1000

    U42-44 modifying reset server

--- back-end/tests/common.py
+++ back-end/tests/common.py
@@ -5,13 +5,8 @@ port = 8080
 url = f"http://localhost:{port}/"
 
 def reset_server():
-    thumbnails = os.path.abspath(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)),'src/imgs/thumbnails')
-    )
     requests.delete(url + 'reset')
-    for file in os.scandir(thumbnails):
-        os.remove(file.path)
-
+    
 def getRecipeData1(recipeId, accountId):
     return {
         "recipeId": recipeId,
